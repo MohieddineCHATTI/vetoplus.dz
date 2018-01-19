92b269a8a5125614e6740773be980e889159bdfa
MohieddineCHATTI <mohieddinechatti@gmail.com>
2018-01-20 00:29:08 +0100

    search and filter by tags created

--- vetoplus.dz.js
+++ vetoplus.dz.js
@@ -13,7 +13,6 @@
             contentType: "application/json",
             crossDomain: true,
             success: function(top_prod){
-            console.log(top_prod[1].nom);
             for (var j=0; j<top_prod.length; j++){
         $("#prod_list").append("<hr><li class='col-sm-12 prod row'><figure class='col-sm-4'><img src='"+top_prod[j].img+"' alt='image du produit' ><figcaption>"+top_prod[j].nom+"</figcaption></figure><p class='col-sm-5'><span class='big_title'>la marque:</span>"+top_prod[j].marque+"<br><span class='big_title'>description: </span><br>"+top_prod[j].description+"</p><p class='col-sm-3'><span class='big_title'>prix: </span> <br><del>"+top_prod[j].exprix +"</del>--<ins>"+top_prod[j].prix+"</ins><br><span class='big_title'>contacter:<br></span> <span class='contact_info'>"+top_prod[j].contact+"</span></p></li><br>");
 
@@ -38,7 +37,6 @@ starting_page();
             contentType: "application/json",
             crossDomain: true,
             success: function(all){
-            console.log(all[1].nom+" "+all[1].nom);
             for (var m=0; m<all.length; m++){
         $("#prod_list").append("<hr><li class='col-sm-12 prod row'><figure class='col-sm-4'><img src='"+all[m].img+"' alt='image du produit' ><figcaption>"+all[m].nom+"</figcaption></figure><p class='col-sm-5'><span class='big_title'>la marque:</span>"+all[m].marque+"<br><span class='big_title'>description: </span><br>"+all[m].description+"</p><p class='col-sm-3'><span class='big_title'>prix: </span> <br><del>"+all[m].exprix +"</del>--<ins>"+all[m].prix+"</ins><br><span class='big_title'>contacter:<br></span> <span class='contact_info'>"+all[m].contact+"</span></p></li><br>");
 
@@ -64,27 +62,27 @@ var all_tags=[];
                 //making tags list
                 for (var i=0; i<all_prod.length; i++){
                         if (!all_tags.includes(all_prod[i].nom)){
-                            $("#tags_list").append("<li>"+all_prod[i].nom+"</li>");
                             all_tags.push(all_prod[i].nom);
                     
                         }
                         if (!all_tags.includes(all_prod[i].categ)){
-                            $("#tags_list").append("<li>"+all_prod[i].categ+"</li>");
                             all_tags.push(all_prod[i].categ);
                     
                         }
                         if (!all_tags.includes(all_prod[i].sous_categ)){
-                            $("#tags_list").append("<li>"+all_prod[i].sous_categ+"</li>");
                             all_tags.push(all_prod[i].sous_categ);
                     
                         }
                         if (!all_tags.includes(all_prod[i].marque)){
-                            $("#tags_list").append("<li>"+all_prod[i].marque+"</li>");
                             all_tags.push(all_prod[i].marque);
                     
                         }
 
 
+                 }
+                 all_tags=all_tags.sort();
+                 for(var s=0; s<all_tags.length; s++){
+                     $("#tags_list").append('<li id="'+all_tags[s]+'"class="tag">'+all_tags[s]+'</li>');
                  }
 
             },
@@ -136,15 +134,10 @@ var all_tags=[];
            if (key_word==""){
             for (f = 0; f < filtred_elements.length; f++) {
                 
-                     filtred_elements[f].style.display = "none";
+                     filtred_elements[f].style.display = "";
                  }
-                 console.log('works');
                 }else{
-
-                console.log('works');
-                console.log(filtred_elements.length);
                 for (f = 0; f < filtred_elements.length; f++) {
-                   console.log('filtering');
                     if (filtred_elements[f].innerHTML.toUpperCase().indexOf(key_word) > -1) {
                         filtred_elements[f].style.display = "";
                     } else {
@@ -154,8 +147,35 @@ var all_tags=[];
             
                     
             }
-             }
+}
+
+
+
+
+
+
+
+function filter_by_tag(tag){
+    $("#prod_list").empty();
+    $.ajax({
+        type: "GET",
+        url: "http://www.vetoplus.net/data.json",
+        dataType: "json",
+        contentType: "application/json",
+        crossDomain: true,
+        success: function(all_prod){
+            for (var t=0; t<all_prod.length; t++){
+                if (all_prod[t].nom==tag || all_prod[t].categ==tag || all_prod[t].sous_categ==tag || all_prod[t].marque==tag){
+                    $("#prod_list").append("<hr><li class='col-sm-12 prod row'><figure class='col-sm-4'><img src='"+all_prod[t].img+"' alt='image du produit' ><figcaption>"+all_prod[t].nom+"</figcaption></figure><p class='col-sm-5'><span class='big_title'>la marque:</span>"+all_prod[t].marque+"<br><span class='big_title'>description :</span><br>"+all_prod[t].description+"</p><p class='col-sm-3'><span class='big_title'>prix:</span><br><del>"+all_prod[t].exprix +"</del>--<ins>"+all_prod[t].prix+"</ins><br><span class='big_title'>contacter: <br>"+all_prod[t].contact+"</p></li><br><hr>");
+                }
+            }
+},
          
-                 
-            
+    });
+};
+var the_tag;
+$(document).delegate('.tag','click',function(){
+    the_tag=$(this).attr('id');
+    filter_by_tag(the_tag);
+});    
         
