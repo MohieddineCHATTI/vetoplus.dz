166846b944b32d0b03e727a2a7aff25f6e1d69f4
MohieddineCHATTI <mohieddinechatti@gmail.com>
2018-01-19 17:15:38 +0100

    changing the filtering idea

--- vetoplus.dz.js
+++ vetoplus.dz.js
@@ -123,9 +123,44 @@ starting_page();
         };
 
 
+        var tst_lst =["sjkfh","qsfjfq","abqsf","skjfhq","zkjfhq"];
+        for (var y=0; y<tst_lst.length; y++){
+            $("#filtred_list ").append("<li style='display:none' class='filt'>"+tst_lst[y]+"</li>");
+        }
+         // function filter by entering input in search field
+         function filter_func() {
+              // Declare variables
+            var search_field ,key_word,filtred_list,filtred_elements, li, a, i;
+            search_field = document.getElementById('search_field');
+            key_word = search_field.value.toUpperCase();
+            filtred_list = document.getElementById("filtred_list");
+            filtred_elements = filtred_list.getElementsByClassName('filt');
 
-         // function filter by sub_category
+            // Loop through all list items, and hide those who don't match the search query
+
+           if (key_word==""){
+            for (f = 0; f < filtred_elements.length; f++) {
+                
+                     filtred_elements[f].style.display = "none";
+                 }
+                 console.log('works');
+                }else{
+
+                console.log('works');
+                console.log(filtred_elements.length);
+                for (f = 0; f < filtred_elements.length; f++) {
+                   console.log('filtering');
+                    if (filtred_elements[f].innerHTML.toUpperCase().indexOf(key_word) > -1) {
+                        filtred_elements[f].style.display = "";
+                    } else {
+                        filtred_elements[f].style.display = "none";
+                    }
+                }
+            
+                    
+            }
+             }
+         
+                 
         
-$('ul').find("p").click(function(){
-    alert('kjfhqs');
-});
+        
